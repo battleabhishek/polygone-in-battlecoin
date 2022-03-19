db2d74bd80012a616d10bbf72a070863c18b94bb
battleabhishek <101720155+battleabhishek@users.noreply.github.com>
2022-03-19 22:21:16 +0530

    Initial commit

---
+++ polygone in battlecoin/README.md
@@ -0,0 +1,3 @@
+# polygone in battlecoin
+
+
---
+++ polygone in battlecoin/dev/JavaScript1.js
@@ -0,0 +1,8 @@
+// Javascript
+
+const Web3 = require('Web3')
+
+// Sign up for a free dedicated RPC URL at https://rpc.maticvigil.com/ or other hosted node providers.
+const web3 = new Web3('https://rpc-mumbai.matic.today')
+
+// web3 object is now connected with Polygon's node
